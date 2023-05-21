e8dbfe604e190ebc183b67be7947346f9975f109
yassinebk <yassinebk23@gmail.com>
2023-05-21 11:17:59 +0100

    feat: initial-commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+import { useColorScheme } from "react-native";
 
+import { Button, Paragraph, Spacer, TamaguiProvider, Theme, YStack } from "tamagui";
+import config from "./tamagui.config";
 export default function App() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+  });
+  if (!loaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TamaguiProvider config={config}>
+      <Theme name={colorScheme === "light" ? "light" : "dark"}>
+        <YStack
+          f={1}
+          jc="center"
+          ai="center"
+          backgroundColor={"$backgroundSoft"}
+        >
+          
+          <Paragraph color="$color" jc="center">
+            {colorScheme}
+          </Paragraph>
+          <Button theme={"alt2"} backgroundColor="$blue10">Meow</Button>
+
+          <StatusBar style="auto" />
+        </YStack>
+      </Theme>
+    </TamaguiProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
